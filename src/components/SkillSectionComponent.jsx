eca8d1330b9c331d8fed1818ddd60f3e5d94ec52
bikrantbdr <bikrantbdr@gmail.com>
2023-10-17 22:28:50 +0545

    completed skills page

---
+++ src/components/SkillSectionComponent.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components"
+import bg1 from '../assets/skill-bg-1.svg'
+import bg2 from '../assets/skill-bg-2.svg'
+import { useState } from "react"
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap:64px;
+  width: 95%;
+  height:100px;
+  rotate: -6deg;
+  position: relative;
+  cursor: pointer;
+
+  background-image: ${props => props.index % 2 === 0 ? `url(${bg1})` : `url(${bg2})`};
+  filter: ${props => props.showSkill ? 'grayscale(20%)' : 'grayscale(100%)'};
+  transition:filter 0.3s ease-in-out;
+  
+  background-size: cover;
+  background-repeat: no-repeat;
+`
+
+const Title = styled.h1`
+  color: rgba(0, 0, 0, 0.20);
+text-align: center;
+font-family: Raleway;
+font-size: 56px;
+font-style: normal;
+font-weight: 800;
+line-height: normal;
+
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+opacity: ${props => props.showSkill ? 0 : 1};
+transition: opacity 1s ease-in-out;
+`
+
+const Icon = styled.img`
+  height: 60%;
+  width: auto;
+  object-fit: contain;
+  filter: grayscale(80%);
+  
+  cursor: pointer;
+  opacity: ${props => props.showSkill ? 1 : 0};
+  transition: opacity 1s ease-in-out;
+
+  &:hover {
+    filter: grayscale(0%);
+    transform: scale(1.1);
+    transition: transform 0.3s ease-in-out;
+  }
+
+`
+
+const SkillSectionComponent = ({skillType,skill, index}) => {
+  const [showSkill, setShowSkill] = useState(false)
+  return (
+    <Container index ={index} onMouseEnter={()=>(setShowSkill(true))} onMouseLeave={()=>(setShowSkill(false))}  showSkill={showSkill}>
+      {console.log(index)}
+      
+        <Title showSkill={showSkill}>
+      {skillType}
+      </Title>
+      
+      {skill.map((item, index) => {
+        return (
+          <Icon src={item.icon} key={index} showSkill={showSkill} />
+        )
+
+      })}
+      
+      
+      
+    </Container>
+  )
+}
+
+export default SkillSectionComponent
--- src/pages/SkillPage.jsx
+++ src/pages/SkillPage.jsx
@@ -1,14 +1,174 @@
-import React from 'react'
+import styled from 'styled-components'
 import { motion } from 'framer-motion'
+import SkillSectionComponent from '../components/skillSectionComponent'
+
+import figma from '../assets/logo/figma.svg'
+import canva from '../assets/logo/canva.svg'
+import html from '../assets/logo/html.svg'
+import css from '../assets/logo/css.svg'
+import javascript from '../assets/logo/javascript.svg'
+import react from '../assets/logo/react.svg'
+import next from '../assets/logo/next.svg'
+import tailwind from '../assets/logo/tailwind.svg'
+import styledcomponent from '../assets/logo/styled.svg'
+import node from '../assets/logo/node.svg'
+import express from '../assets/logo/express.svg'
+import mongodb from '../assets/logo/mongodb.svg'
+import postman from '../assets/logo/postman.svg'
+import git from '../assets/logo/git.svg'
+import github from '../assets/logo/github.svg'
+import vscode from '../assets/logo/vscode.svg'
+
+const skillSet = [
+  {
+    skillType: "UI/UX",
+    skill: [
+      {
+        name: "Figma",
+        icon: figma,
+      },
+      {
+        name:'canva',
+        icon: canva,
+      }
+    ],
+  },
+  {
+    skillType: "Frontend",
+    skill: [
+      {
+        name: "HTML",
+        icon: html,
+      },
+      {
+        name: "CSS",
+        icon: css,
+      },
+      {
+        name: "JavaScript",
+        icon: javascript,
+      },
+      {
+        name: "React",
+        icon: react,
+      },
+      {
+        name: "Next.js",
+        icon: next,
+      },
+      {
+        name: "Tailwind",
+        icon: tailwind,
+      },
+      {
+        name: "Styled-Component",
+        icon: styledcomponent,
+      },
+    ],
+  },
+  {
+    skillType: "Backend",
+    skill: [
+      {
+        name: "Node.js",
+        icon: node,
+      },
+      {
+        name: "Express.js",
+        icon: express,
+      },
+      {
+        name: "MongoDB",
+        icon: mongodb,
+      },
+      {
+        name: "Postman",
+        icon: postman,
+      },
+    ],
+  },
+  {
+    skillType: "Tools",
+    skill: [
+      {
+        name: "Git",
+        icon: git,
+      },
+      {
+        name: "Github",
+        icon: github,
+      },
+      {
+        name: "VSCode",
+        icon: vscode,
+      },
+    ],
+  },
+
+]
+
+
+const Container = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 80px 0;
+  min-height: 100vh;
+  background-color: #f9f9f9;
+  user-select: none;
+`
+const Title = styled.h1`
+  color: #000;
+  font-family: Pacifico;
+  font-size: 46px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.2px;
+  top: 20px;
+  left: 20px;
+  position:fixed;
+`
+const Subtitle = styled.h2`
+  color: #b3b3b370;
+  z-index: 0;
+  font-family: Raleway;
+  font-size: 38px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  letter-spacing: 1.33px;
+  position:fixed;
+  top: 80px;
+  left: 20px;
+
+`
 
 const SkillPage = () => {
   return (
-    <motion.div
+    <Container
       initial={{ y: window.innerHeight }}
       animate={{ y: 0 }}
       transition={{ type: 'spring', duration: 1.5, delay: 0.5 }}
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
-    >SkillPage</motion.div>
+    >
+      <Title>Skills</Title>
+      <Subtitle>Learning everyday💡</Subtitle>
+
+      {skillSet.map(({ skillType, skill },index) => (
+        <SkillSectionComponent
+          key={skillType}
+          skillType={skillType}
+          skill={skill}
+          index={index}
+        />
+      ))}
+          
+        
+
+    </Container>
   )
 }
 
